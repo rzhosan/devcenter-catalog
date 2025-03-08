c6170a7cd4b80e35bd8db04541023f651afce32c
Rostislav Zhosan <rostislav.zhosan@gmail.com>
2025-03-08 11:26:53 +0100

    Forward and backward lines replacement

--- scripts/replace_lines.ps1
+++ scripts/replace_lines.ps1
@@ -1,2 +1,25 @@
-(Get-Content "src/chocolatey.resources/helpers/functions/Get-WebFileName.ps1") -replace "New-Object System.Net.WebClient", "New-Object System.Net.WebClient; `$webClient.Proxy = `$null" | Set-Content "src/chocolatey.resources/helpers/functions/Get-WebFileName.ps1"
-(Get-Content "src/chocolatey.resources/helpers/functions/Get-WebHeaders.ps1") -replace "New-Object System.Net.WebClient", "New-Object System.Net.WebClient; `$webClient.Proxy = `$null" | Set-Content "src/chocolatey.resources/helpers/functions/Get-WebHeaders.ps1"
+(Get-Content 'C:\ProgramData\chocolatey/helpers/functions/Get-WebFile.ps1') `
+    -replace 'New-Object System.Net.WebClient', 'New-Object System.Net.WebClient; `$webclient.Proxy = `$null' `
+    | Set-Content 'C:\ProgramData\chocolatey/helpers/functions/Get-WebFile.ps1';
+
+(Get-Content 'C:\ProgramData\chocolatey/helpers/functions/Get-WebFileName.ps1') `
+    -replace 'New-Object System.Net.WebClient', 'New-Object System.Net.WebClient; `$client.Proxy = `$null' `
+    | Set-Content 'C:\ProgramData\chocolatey/helpers/functions/Get-WebFileName.ps1';
+
+(Get-Content 'C:\ProgramData\chocolatey/helpers/functions/Get-WebHeaders.ps1') `
+    -replace 'New-Object System.Net.WebClient', 'New-Object System.Net.WebClient; `$client.Proxy = `$null' `
+    | Set-Content 'C:\ProgramData\chocolatey/helpers/functions/Get-WebHeaders.ps1';
+
+# Replace back
+
+(Get-Content 'C:\ProgramData\chocolatey/helpers/functions/Get-WebFile.ps1') `
+    -replace 'New-Object System.Net.WebClient; `$webclient.Proxy = `$null', 'New-Object System.Net.WebClient' `
+    | Set-Content 'C:\ProgramData\chocolatey/helpers/functions/Get-WebFile.ps1';
+
+(Get-Content 'C:\ProgramData\chocolatey/helpers/functions/Get-WebFileName.ps1') `
+    -replace 'New-Object System.Net.WebClient; `$client.Proxy = `$null', 'New-Object System.Net.WebClient' `
+    | Set-Content 'C:\ProgramData\chocolatey/helpers/functions/Get-WebFileName.ps1';
+
+(Get-Content 'C:\ProgramData\chocolatey/helpers/functions/Get-WebHeaders.ps1') `
+    -replace 'New-Object System.Net.WebClient; `$client.Proxy = `$null', 'New-Object System.Net.WebClient' `
+    | Set-Content 'C:\ProgramData\chocolatey/helpers/functions/Get-WebHeaders.ps1';
